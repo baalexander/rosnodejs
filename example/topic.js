5dbfe60a2a0f4e5c4acc3b3b9bb1bb5baecfe4ef
Brandon Alexander <baalexander@gmail.com>
2011-05-24 22:54:11 -0500

    Adds Subscriber and some event handling for Slave API.

--- example/topic.js
+++ example/topic.js
@@ -1,7 +1,11 @@
 var ros = require('../lib/ros')
 
 var node = ros.createNode('talker')
-console.log(node)
+
+var subscriber = node.createSubscriber()
+subscriber.subscribe('chatter', function(message) {
+  console.log('SUBSCRIBEDMESSAGE: ' + message)
+})
 
 var publisher = node.createPublisher()
 publisher.publish('chatter', 'hi!')
