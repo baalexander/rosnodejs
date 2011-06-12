521cffac9f978ffbabe01feb67e0bcce646a562b
Brandon Alexander <baalexander@gmail.com>
2011-06-12 15:17:30 -0500

    Adds register/unregister Subscriber support.

--- example/topic.js
+++ example/topic.js
@@ -3,10 +3,12 @@ var ros = require('../lib/ros')
 var node = ros.createNode('talker')
 
 var subscriber = node.createSubscriber()
-subscriber.subscribe('chatter', function(message) {
-  console.log('SUBSCRIBEDMESSAGE: ' + message)
+subscriber.subscribe('chatter', function(error, message) {
+
 })
 
 var publisher = node.createPublisher()
-publisher.publish('chatter', { data:'hi!', type:'std_msgs/String' })
+publisher.publish('chatter', 'hi!')
 
+publisher.unregister('chatter')
+subscriber.unregister('chatter')
