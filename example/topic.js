718362493c66bc63a4cb845f9b60c32dcfc1915b
Brandon Alexander <baalexander@gmail.com>
2011-06-12 22:07:25 -0500

    Adds examples for using non-topic Master calls.

--- example/topic.js
+++ example/topic.js
@@ -1,6 +1,7 @@
-var ros = require('../lib/ros')
+var ros    = require('../lib/ros')
 
-var node = ros.createNode('talker')
+var master = ros.getMaster()
+var node   = ros.createNode('talker')
 
 var subscriber = node.createSubscriber()
 subscriber.subscribe('chatter', function(error, message) {
@@ -9,6 +10,16 @@ subscriber.subscribe('chatter', function(error, message) {
 
 var publisher = node.createPublisher()
 publisher.publish('chatter', 'hi!')
+
+master.getSystemState('/test', function(error, value) {
+  console.log('SYSTEM STATE:')
+  console.log(value)
+})
+
+master.lookupNode('/test', 'talker', function(error, value) {
+  console.log('LOOKUP NODE:')
+  console.log(value)
+})
 
 publisher.unregister('chatter')
 subscriber.unregister('chatter')
