17b045bc7f1ffc65b69a457ce0640af63eb69f4f
Brandon Alexander <baalexander@gmail.com>
2012-03-24 23:30:41 -0500

    Uses Mocha for test cases.
    Fixes a global var found by mocha.

--- test/message-test.js
+++ test/message-test.js
@@ -1,159 +1,144 @@
-var assert = require('assert'),
-    messages = require('../lib/messages');
+var path     = require('path')
+  , should   = require('should')
+  , messages = require('../lib/messages')
+  ;
 
-var tests = {
-    'When I ask for unexisting file to be parse' : function(next){
-        var then="I got a UnexistingFile error";
-        
-        messages.parseMessageFile("UnexistingFile", function(err, properties, hash) {
-            assert.equal(err.code , 'ENOENT')
-            next(then);
-        });
-    },
-    'When I ask for an empty file to be parse' : function(next){
-        var then="thereis no error";
-        
-        messages.parseMessageFile("/dev/null", function(err, properties, hash) {
+describe('Messages', function() {
 
-            assert.equal(err , null);
-            assert.equal(properties.length , 0);
-            assert.equal(hash,'d41d8cd98f00b204e9800998ecf8427e');
-            next(then);
-        });
-    },
-    'When I ask for a valid file to be parse' : function(next){
-        var then="the files is correctly parsed"
-        
-        messages.parseMessageFile("./msg/test.msg", function(err, properties, hash) {
-            assert.equal(err , null);
-            assert.equal(properties.length , 14);
-            assert.equal(typeof properties[0], 'object');
-            
-            assert.equal(properties[0].type , "bool");
-            assert.equal(properties[0].name , "auto_disable_bodies");
+  describe('parsing', function() {
+    it('should error on an unexisting file', function(done) {
+      messages.parseMessageFile('UnexistingFile', function(error, properties, hash) {
+        error.code.should.equal('ENOENT');
+        done();
+      });
+    });
 
-            assert.equal(properties[7].type , "uint32");
-            assert.equal(properties[7].name , "max_contacts");
+    it('should handle an empty file', function(done) {
+      messages.parseMessageFile('/dev/null', function(error, properties, hash) {
+        should.not.exist(error);
+        properties.length.should.equal(0);
+        hash.should.equal('d41d8cd98f00b204e9800998ecf8427e');
+        done();
+      });
+    });
 
-            assert.equal(properties[11].type , "int32");
-            assert.equal(properties[11].name , "Y");
-            assert.equal(properties[11].value , "-123");
+    it('should parse a valid message definition file', function(done) {
+      var messagePath = path.join(__dirname, 'msg', 'test.msg');
+      messages.parseMessageFile(messagePath, function(error, properties, hash) {
+        should.not.exist(error);
+        properties.length.should.equal(14);
 
-            assert.equal(properties[12].type , "string");
-            assert.equal(properties[12].name , "FOO");
-            assert.equal(properties[12].value , "foo");
+        properties[0].should.be.a('object');
+        properties[0].type.should.equal('bool');
+        properties[0].name.should.equal('auto_disable_bodies');
 
-            assert.equal(properties[13].type , "string");
-            assert.equal(properties[13].name , "EXAMPLE");
-            assert.equal(properties[13].value , '"#comments" are ignored, and leading and trailing whitespace removed');
-            next(then);
-        });
-    },
-    'When I ask registration of message from local file' : function(next){
-        var then="The file is correctly readed and parsed";
-      
-        messages.registerMessageFromFile("test", function(err, messages) {
+        properties[7].type.should.equal('uint32');
+        properties[7].name.should.equal('max_contacts');
 
-            assert.equal( messages.id , "test");
-            assert.equal( messages.messageName , "test");
-            assert.equal( messages.className , "Test");
-            assert.equal( messages.properties[0].name , "auto_disable_bodies");
-            next(then);
-        });
-    },
-    'When I ask registration of message from packaged file' : function(next){
-        then="I get the good properties"
+        properties[11].type.should.equal('int32');
+        properties[11].name.should.equal('Y');
+        properties[11].value.should.equal('-123');
 
-        messages.registerMessageFromFile("Pose","turtlesim", function(err, messages) {
-            assert.equal( messages.id , "turtlesim/Pose");
-            assert.equal( messages.messageName , "Pose");
-            assert.equal( messages.className , "Pose");
-            assert.equal( messages.properties[0].name , "x");
-            next(then);
-        });
-    },
-    'When I ask for a Object build from a packaged message definition': function(next){
-        var then = 'The returned object is initlaized with the object parameter';
+        properties[12].type.should.equal('string');
+        properties[12].name.should.equal('FOO');
+        properties[12].value.should.equal('foo');
 
-        messages.createNewMessageFromFile("Pose","turtlesim", null,function(err, ObjectMessage) {
-            assert.notEqual(ObjectMessage.x ,undefined);
-            assert.notEqual(ObjectMessage.y ,undefined);
-            assert.notEqual(ObjectMessage.linearVelocity ,undefined , "linearVelocity is undefined");
-            assert.notEqual(ObjectMessage.angularVelocity ,undefined , "angularVelocity is undefined");
-            assert.equal(ObjectMessage.angular_velocity ,undefined , "angular_velocity is not undefined");
-            next(then);
-        });
-    },
-    'When I ask for a Object build from a packaged message definition with constantes': function(next){
-        var then = 'Constants are correctly initialized';
+        properties[13].type.should.equal('string');
+        properties[13].name.should.equal('EXAMPLE');
+        properties[13].value.should.equal('"#comments" are ignored, and leading and trailing whitespace removed');
+        done();
+      });
+    });
+  });
 
-        messages.createNewMessageFromFile("Constants","bond", null, function(err, ObjectMessage) {
-            assert.notEqual(ObjectMessage.DEFAULT_CONNECT_TIMEOUT, undefined);
-            assert.equal(ObjectMessage.DEFAULT_CONNECT_TIMEOUT, 10);
-            assert.equal(ObjectMessage.DISABLE_HEARTBEAT_TIMEOUT_PARAM , '/bond_disable_heartbeat_timeout');
-            next(then);
-        });
-    },
-    'When I ask for a Object build from a packaged message definition with init values': function(next){
-        var then = 'Init values are correctly added';
+  describe('registry', function() {
+    it('should register a message defined in a local message file', function(done) {
+      messages.registerMessageFromFile('test', function(error, message) {
+        should.not.exist(error);
+        message.id.should.equal('test');
+        message.messageName.should.equal('test');
+        message.className.should.equal('Test');
+        message.properties[0].name.should.equal('auto_disable_bodies');
+        done();
+      });
+    });
 
-        messages.createNewMessageFromFile("Pose","turtlesim", {y:55, linearVelocity:3}, function(err, ObjectMessage) {
-            console.log(ObjectMessage);
-            assert.notEqual(ObjectMessage.x ,undefined);
-            assert.equal(ObjectMessage.y ,55);  
-            assert.equal(ObjectMessage.linearVelocity ,3 );
-            next(then);
-        });
-    },
-    'When I ask an object  build from message from local file' : function(next){
-        var then="The created object is valid in all mode";
-        
-        messages.createNewMessageFromFile("test","" , null ,  function(err, ObjectMessage) {
-            console.log(ObjectMessage);
-            assert.equal(ObjectMessage.validate(ObjectMessage), true);
-            assert.equal(ObjectMessage.validate(ObjectMessage, true), true);
-            next(then);
-        });
-    },
-    'When I ask an object  build from message from local file with init valid' : function(next){
-        var then="The created object is valid";
-        
-        messages.createNewMessageFromFile("test","" , {contactMaxCorrectingVel:"TESTVAL"} ,  function(err, ObjectMessage) {
-            assert.equal(ObjectMessage.validate(ObjectMessage), true);
-            assert.equal(ObjectMessage.validate(ObjectMessage, true), true);
-            next(then);
-        });
-    },
-    'When I ask an object  build from message from local file with init adding some valued' : function(next){
-        var then="The created object is not valid in strict mode";
-        
-        messages.createNewMessageFromFile("test","" , {newValues:"TESTVAL"} ,  function(err, ObjectMessage) {
-            assert.equal(ObjectMessage.validate(ObjectMessage), true);
-            assert.equal(ObjectMessage.validate(ObjectMessage, false), true);
-            next(then);
-        });
-    }
-};
+    it('should register a message defined in a package\'s message file', function(done) {
+      messages.registerMessageFromFile('Pose', 'turtlesim', function(error, message) {
+        message.id.should.equal('turtlesim/Pose');
+        message.messageName.should.equal('Pose');
+        message.className.should.equal('Pose');
+        message.properties[0].name.should.equal('x');
+        done();
+      });
+    });
+  });
 
-var testArray=[];
-var testName=[];
-for(var test in tests){
-    testArray.push(tests[test]);
-    testName.push(test);
-}
-var testIndex=0;
-var timeout;
-var nextTest=function(message){
-    if(timeout)
-        clearTimeout(timeout);
-   
-if(testIndex>0){
-       console.log("\033[32m\t"+(message || "It works")+"\033[39m");
-}
-    if(testArray.length>testIndex){
-        console.log(testName[testIndex]);
-        timeout= setTimeout(function(){ assert.fail("<5000" , ">5000" , "The test is too long")} , 5000); 
-        testArray[testIndex++](nextTest);
-    }
-};
-nextTest();
+  describe('creation', function() {
+    it('should initialize a message defined in a package', function(done) {
+      messages.createNewMessageFromFile('Pose', 'turtlesim', null, function(error, message) {
+        message.should.have.property('x');
+        message.should.have.property('y');
+        message.should.have.property('linearVelocity');
+        message.should.have.property('angularVelocity');
+        message.should.not.have.property('angular_velocity');
+        done();
+      });
+    });
+
+    it('should initialize a message containing constants', function(done) {
+      messages.createNewMessageFromFile('Constants', 'bond', null, function(error, message) {
+        message.should.have.property('DEFAULT_CONNECT_TIMEOUT');
+        message.DEFAULT_CONNECT_TIMEOUT.should.equal(10.0);
+        message.DISABLE_HEARTBEAT_TIMEOUT_PARAM.should.equal('/bond_disable_heartbeat_timeout');
+        done();
+      });
+    });
+
+    it('should initialize a message with initial values', function(done) {
+      var values = {
+        y: 55
+      , linearVelocity: 3
+      }
+      messages.createNewMessageFromFile('Pose', 'turtlesim', values, function(error, message) {
+        message.should.have.property('x');
+        message.y.should.equal(55);
+        message.linearVelocity.should.equal(3);
+        done();
+      });
+    });
+
+    it('should initialize a valid message defined in a local file', function(done) {
+      messages.createNewMessageFromFile('test', '', null,  function(error, message) {
+        message.validate(message).should.be.true;
+        message.validate(message, true).should.be.true;
+        done();
+      });
+    });
+
+    it('should initialize a valid message with initial values', function(done) {
+      var values = {
+        contactMaxCorrectingVel : 'TESTVAL'
+      }
+      messages.createNewMessageFromFile('test', '', values, function(error, message) {
+        message.validate(message).should.be.true;
+        message.validate(message, true).should.be.true;
+        done();
+      });
+    });
+
+    it('should initialize a valid message that fails strict mode with initial values', function(done) {
+      var values = {
+        newValues: 'TESTVAL'
+      }
+      messages.createNewMessageFromFile('test', '', values, function(error, message) {
+        message.validate(message).should.be.true;
+        message.validate(message, false).should.be.true;
+        message.validate(message, true).should.be.false;
+        done();
+      });
+    });
+  });
+
+});
+
