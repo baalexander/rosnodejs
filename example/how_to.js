e1e69fc22b958230d3069bbe3b6e2635fa2080d6
Brandon Alexander <baalexander@gmail.com>
2012-04-12 22:26:57 -0500

    Can subscribe to basic messages. But fires twice.

--- example/how_to.js
+++ example/how_to.js
@@ -3,7 +3,22 @@ var should = require('should')
 
 describe('How to use rosnodejs', function() {
 
-  it('to publish messages', function(done) {
+  // it('to publish messages', function(done) {
+  //   ros.types([
+  //     'std_msgs/String'
+  //   ], function(String) {
+  //     var node = ros.node('talker');
+  //     node.topics([
+  //       { topic: 'hello_world', messageType: String }
+  //     ], function(helloWorld) {
+  //       var message = new String({ data: 'hello world!' });
+  //       helloWorld.publish(message);
+  //       setTimeout(done, 1500);
+  //     });
+  //   });
+  // });
+
+  it('to subscribe to messages', function(done) {
     ros.types([
       'std_msgs/String'
     ], function(String) {
@@ -11,12 +26,13 @@ describe('How to use rosnodejs', function() {
       node.topics([
         { topic: 'hello_world', messageType: String }
       ], function(helloWorld) {
-        console.log(String);
-        var message = new String({ data: 'hello world!' });
-        helloWorld.publish(message);
-        setTimeout(done, 1500);
+        helloWorld.subscribe(function(message) {
+          console.log(message);
+        });
+        setTimeout(done, 15000);
       });
     });
   });
 
-})
+});
+
