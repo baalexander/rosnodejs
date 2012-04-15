4436458c4d288f38fa345d54e58f175a5e0128a2
Brandon Alexander <baalexander@gmail.com>
2012-04-14 23:21:23 -0500

    Updates example code to use ROS command line.
    No need to run `rostopic echo` or `rostopic pub` separately when running the
    examples.

--- example/how_to.js
+++ example/how_to.js
@@ -1,34 +1,58 @@
-var should = require('should')
+var exec   = require('child_process').exec
+  , should = require('should')
   , ros    = require('../lib/ros')
 
 describe('How to use rosnodejs', function() {
 
-  // it('to publish messages', function(done) {
-  //   ros.types([
-  //     'std_msgs/String'
-  //   ], function(String) {
-  //     var node = ros.node('talker');
-  //     node.topics([
-  //       { topic: 'hello_world', messageType: String }
-  //     ], function(helloWorld) {
-  //       var message = new String({ data: 'howdy' });
-  //       helloWorld.publish(message);
-  //       setTimeout(done, 1500);
-  //     });
-  //   });
-  // });
+  it('to publish messages', function(done) {
+    this.timeout(5000);
 
-  it('to subscribe to messages', function(done) {
     ros.types([
       'std_msgs/String'
     ], function(String) {
       var node = ros.node('talker');
       node.topics([
-        { topic: 'hello_world', messageType: String }
-      ], function(helloWorld) {
-        helloWorld.subscribe(function(message) {
+        { topic: 'publish_example', messageType: String }
+      ], function(publishExample) {
+        // Uses the ROS command line tool rostopic to echo messages published
+        // over the 'publish_example' topic.
+        var subscribeCommand = 'rostopic'
+          + ' echo'
+          + ' /publish_example';
+        var child = exec(subscribeCommand, function(error, stdout, stderr) {
+          should.not.exist(error);
+        });
+
+        var message = new String({ data: 'howdy' });
+        publishExample.publish(message);
+        setTimeout(done, 1500);
+      });
+    });
+  });
+
+  it('to subscribe to messages', function(done) {
+    this.timeout(5000);
+
+    ros.types([
+      'std_msgs/String'
+    ], function(String) {
+      var node = ros.node('listener');
+      node.topics([
+        { topic: 'subscribe_example', messageType: String }
+      ], function(subscribeExample) {
+        subscribeExample.subscribe(function(message) {
           message.data.should.equal('howdy');
-          done()
+          done();
+        });
+
+        // Uses rostopic to publish a message on the subscribed to topic.
+        var publishCommand = 'rostopic'
+          + ' pub'
+          + ' /subscribe_example'
+          + ' std_msgs/String'
+          + ' howdy';
+        var child = exec(publishCommand, function(error, stdout, stderr) {
+          should.not.exist(error);
         });
       });
     });
