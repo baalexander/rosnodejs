1ac849104412c4c111059c9976bfa349fd9c217a
Brandon Alexander <baalexander@gmail.com>
2012-04-24 22:04:27 -0500

    Adds unpublish function to publisher.
    Unregisters publisher as a publisher of a given topic with master. Unpublish may
    be an ambiguous name though.
    
    See Issue #11.

--- example/how_to.js
+++ example/how_to.js
@@ -1,37 +1,47 @@
 var exec   = require('child_process').exec
   , should = require('should')
   , ros    = require('../lib/ros')
+  ;
 
 describe('How to use rosnodejs', function() {
 
-  // it('to publish messages', function(done) {
-  //   this.timeout(5000);
+  it('to publish messages', function(done) {
+    this.timeout(5000);
 
-  //   ros.types([
-  //     'std_msgs/String'
-  //   ], function(String) {
-  //     // Uses the ROS command line tool rostopic to echo messages published
-  //     // over the 'publish_example' topic.
-  //     var subscribecommand = 'rostopic'
-  //       + ' echo'
-  //       + ' /publish_example';
-  //     var child = exec(subscribecommand, function(error, stdout, stderr) {
-  //       should.not.exist(error);
-  //     });
+    ros.types([
+      'std_msgs/String'
+    ], function(String) {
 
-  //     // Creates the topic 'publish_example'
-  //     var publishExample = new ros.topic({
-  //       node        : 'talker'
-  //     , topic       : 'publish_example'
-  //     , messageType : String
-  //     });
+      // Uses the ROS command line tool rostopic to echo messages published
+      // over the 'publish_example' topic.
+      var subscribeCommand = 'rostopic'
+        + ' echo'
+        + ' /publish_example'
+        + ' -n 1'
+        ;
+      var child = exec(subscribeCommand, function(error, stdout, stderr) {
+        should.not.exist(error);
+      });
 
-  //     // Sends a std_msgs/String message over the 'publish_example' topic.
-  //     var message = new String({ data: 'howdy' });
-  //     publishExample.publish(message);
-  //     setTimeout(done, 1500);
-  //   });
-  // });
+      // Creates the topic 'publish_example'
+      var publisher = new ros.topic({
+        node        : 'talker'
+      , topic       : 'publish_example'
+      , messageType : String
+      });
+
+      publisher.on('unregistered_publisher', done);
+
+      // Sends a std_msgs/String message over the 'publish_example' topic.
+      var message = new String({ data: 'howdy' });
+      publisher.publish(message);
+
+      // Unregister as a publisher for test clean up
+      setTimeout(function() {
+        publisher.unpublish();
+      }, 1000);
+    });
+  });
 
   it('to subscribe to messages', function(done) {
     this.timeout(5000);
@@ -39,6 +49,7 @@ describe('How to use rosnodejs', function() {
     ros.types([
       'std_msgs/String'
     ], function(String) {
+
       // Creates the topic 'subscribe_example'
       var subscriber = new ros.topic({
         node        : 'listener'
@@ -46,13 +57,13 @@ describe('How to use rosnodejs', function() {
       , messageType : String
       });
 
-      subscriber.on('unregistered_subscriber', function() {
-        done();
-      });
+      subscriber.on('unregistered_subscriber', done);
 
       // Subscribes to the 'subscribe_example' topic
       subscriber.subscribe(function(message) {
         message.data.should.equal('howdy');
+
+        // Unregister as a subscriber for test cleanup
         subscriber.unsubscribe();
       });
 
@@ -62,7 +73,8 @@ describe('How to use rosnodejs', function() {
         + ' /subscribe_example'
         + ' std_msgs/String'
         + ' howdy'
-        + ' --once';
+        + ' --once'
+        ;
       var child = exec(publishCommand, function(error, stdout, stderr) {
         should.not.exist(error);
       });
